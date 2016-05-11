3689117cb969aaa31e2fd54cc27790364d55a013
Lethiel117 <jose.vieira.peres@gmail.com>
2016-05-11 18:34:06 +0100

    Tester tester

---
+++ Assets/Scripts/Barrel.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrel : MonoBehaviour {
+	//randomizers
+	private int randomladder;
+	private int randomBarrel;
+	private int randMaxforladder=6;
+	private int randMinforladder=0;
+	private int randMaxforbarrel=9;
+	private int randMinforbarrel=0;
+	private int descendLadder= 2;
+
+//	private int lerpspeed;
+//
+//	private float lerpfactor;
+//	private float startPos;
+//	private float endPos;
+//
+//	private Vector2 lerpMovement;
+//	private Vector2 currentPos;
+//	private Vector2 destination;
+//	public Vector2 currentplatform;
+//
+	private Collider2D rb;
+	private bool isWild;
+	private bool islessWild;
+
+	void Start () {
+		rb = GetComponent<Collider2D> ();
+	}
+	
+
+	void Update () {
+//			currentPos = transform.position;
+//
+//			lerpfactor += Time.deltaTime* lerpspeed;
+//			lerpMovement = Vector2.Lerp (currentPos,destination,lerpfactor);
+//			transform.position = lerpMovement;
+	}
+
+	void Ontrigger2D(Collider2D other) {
+		if(other.CompareTag ("ladder")){
+			Debug.Log ("triggered");
+			randomladder = Random.Range (randMinforladder,randMaxforladder);
+		}
+		if(randomladder < descendLadder) {
+			rb.enabled = false ;
+				Debug.Log ("down it goes!!!");
+				}
+				else{
+			randomladder--;
+					//watevs cool man
+				}
+		if(other.CompareTag ("brokenladder")){
+			Debug.Log ("triggered");
+			randomladder = Random.Range (randMinforladder,randMaxforladder);
+		}
+		if(randomladder < descendLadder) {
+			rb.enabled = false ;
+			Debug.Log ("down it goes!!!");
+		}
+		else{
+			randomladder--;
+			//watevs cool man
+		}
+		}
+
+		void OntriggerExit2D (Collider2D other){
+		if(other.CompareTag ("ladder")){
+		rb.enabled = true;
+		}
+		}
+
+	void BarrelType() {
+				
+				randomBarrel = Random.Range (randMinforbarrel, randMaxforbarrel);
+		if (randomBarrel == 1) {
+			isWild = true;
+			rb.enabled = false;
+		}
+				else if (randomBarrel == 2)  {
+			islessWild = true;
+			//interacts with oil barrel to spawn flamy.
+				}
+	}
+
+
+}
---
+++ Assets/Scripts/CharColliderScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharColliderScript : MonoBehaviour 
+{
+
+	// PUBLIC
+
+	public float climbSpeed;
+
+	// PRIVATE
+
+	private Rigidbody2D playerRigidbody;
+	private float climbVelocity;
+	private float gravityStore;
+	private bool onLadder;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerRigidbody = GetComponent<Rigidbody2D> ();
+		gravityStore = playerRigidbody.gravityScale;
+	}
+
+	void Update()
+	{
+		if (onLadder) 
+		{
+			playerRigidbody.gravityScale = 0f;
+
+			climbVelocity = climbSpeed * Input.GetAxisRaw ("Vertical");
+
+			playerRigidbody.velocity = new Vector2 (playerRigidbody.velocity.x, climbVelocity);
+		}
+
+		if (!onLadder) 
+		{
+			playerRigidbody.gravityScale = gravityStore;
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other) 
+	{
+		if (other.CompareTag ("ladder")) 
+		{
+			onLadder = true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other) 
+	{
+		if (other.CompareTag ("ladder")) 
+		{
+			onLadder = false;
+		}
+	}
+}
