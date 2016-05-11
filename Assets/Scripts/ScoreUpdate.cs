149c40df8ef5d78f2b418edd5cb23d77242e3b1c
xmota360 <aluno@S08PC02.local>
2016-05-11 19:11:26 +0100

    Master Scene
    Master Scene created

---
+++ Assets/Scripts/ScoreUpdate.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreUpdate : MonoBehaviour 
+{
+	public Image life1;
+	public Image life2;
+	public Image life3;
+	public Text uiScoreText;
+	public Text uiHighScoreText;
+	public Text uiBonusScoreText;
+
+	private int score;
+	private bool haslife1;
+	private bool haslife2;
+	private bool haslife3;
+
+
+
+	void Awake () 
+	{
+		score = 0;
+
+		haslife1 = true;
+		haslife2 = true;
+		haslife3 = true;	
+	}
+	
+
+	void Update () 
+	{
+	
+	}
+}
